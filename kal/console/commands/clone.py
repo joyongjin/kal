146510aa1da7505e274ddf7b3b59eb0a4abbe372
kyle <wnrhd114@gmail.com>
2020-05-28 17:31:02 +0900

    only url option

--- kal/console/commands/clone.py
+++ kal/console/commands/clone.py
@@ -1,19 +1,26 @@
-from cleo import argument
+from cleo import argument, option
 
 from kal.github import url_parsing
 from .base import Command
 
 
 class CloneCommand(Command):
+
     name = 'clone'
     description = 'clone repository from github'
     arguments = [
         argument('repository', 'name of repository that you want to clone'),
         argument('target', 'target directory name', optional=True),
     ]
+    options = [
+        option('url', 'u', 'echo only url, not clone')
+    ]
 
     def handle(self):
         clone_url = url_parsing(self.argument('repository'))
+        if self.option('url'):
+            self.line(clone_url)
+            return 1
         command = 'git clone {}'.format(clone_url)
         if self.argument('target'):
             command += ' {}'.format(self.argument('target'))
