c1a4057f5f32b3b655fc322873db7b5d623580ef
kyle <wnrhd114@gmail.com>
2020-06-03 18:45:38 +0900

    install command 구현중

--- kal/commands/box.py
+++ kal/commands/box.py
@@ -6,7 +6,7 @@ from kal.env import path
 
 """
 link # link all ln script
-
+git # git settings
 """
 
 LINK_PATH = {
@@ -17,6 +17,31 @@ LINK_PATH = {
     'aws': ('keys/.aws', '$HOME/'),
     'terminator': ('settings/terminator', '$HOME/.config/'),
 }
+
+GIT_SETTINGS_COMMANDS = """
+git config --global alias.co commit
+git config --global alias.ad add
+git config --global alias.aa 'add --all'
+git config --global alias.cf config
+git config --global alias.cm 'commit -m'
+git config --global alias.ss 'status --short'
+git config --global alias.s status
+git config --global alias.st stash
+git config --global alias.rs reset
+git config --global alias.rb rebase
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.rt remote
+git config --global alias.us 'reset HEAD --'
+git config --global alias.ca 'commit --amend'
+git config --global alias.adog 'log --all --oneline --decorate --graph'
+git config --global core.excludesfile ~/Dropbox/settings/.gitignore_global/.gitignore_global
+git config --global alias.ch checkout
+git config --global alias.br branch
+git config --global core.editor vim
+git config --global user.email 'wnrhd114@gmail.com'
+git config --global user.name 'joyongjin'
+git config --global color.ui true
+""".strip().split("\n")
 
 
 class BoxCommand(Command):
@@ -38,6 +63,9 @@ class BoxCommand(Command):
         if command == 'link':
             args = self.check_args(1)
             self.link(args[0])
+        if command == 'git':
+            self.check_args(0)
+            self.git()
         else:
             raise CommandError('Invalid command')
 
@@ -48,3 +76,7 @@ class BoxCommand(Command):
             link_path[1],
         )
         self.shell_call(sc)
+
+    def git(self):
+        for c in GIT_SETTINGS_COMMANDS:
+            self.shell_call(c)
---
+++ kal/commands/install.py
@@ -0,0 +1,26 @@
+from cleo import argument
+
+from kal.core.base_commands import Command
+
+INSTALL_DOCKER = """
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo gpasswd -a $USER docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+""".strip().split("\n")
+
+
+class InstallCommand(Command):
+    name = 'install'
+    description = 'Install some program'
+    arguments = [
+        argument('name', 'name of program to install')
+    ]
+
+    def handle(self):
+        self.argument('name')
