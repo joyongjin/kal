f93ac97675f0e8ca0dd78efd664e5c6f87678551
jrog612 <wnrhd114@gmail.com>
2023-09-19 15:29:56 +0900

    remove version and add meta module

--- kal/application.py
+++ kal/application.py
@@ -1,6 +1,5 @@
 from cleo import Application as BaseApplication
 
-from kal import __version__
 from kal.initialize import init
 from . import commands as cmds
 
@@ -8,7 +7,7 @@ from . import commands as cmds
 class Application(BaseApplication):
     def __init__(self, *args, **kwargs):
         init()
-        super().__init__('kal', __version__)
+        super().__init__('kal', meta.__version__)
         for command in self.get_default_commands():
             self.add(command)
 
