50053a22850dae9a7e23de7d3b6153385a255ec5
jrog612 <wnrhd114@gmail.com>
2023-09-19 18:39:24 +0900

    fix typo

--- kal/meta.py
+++ kal/meta.py
@@ -8,4 +8,4 @@ try:
         pyproject = tomllib.load(f)
     __version__ = pyproject["tool"]["poetry"]["version"]
 except Exception as e:
-    __version__ = importlib.metadata.version('bop')
+    __version__ = importlib.metadata.version('kal')
